9b7da389d6e1faaba60740912e82cd9f23ba5743
BrianKheng <briankheng63@gmail.com>
2023-05-05 19:32:59 +0700

    feat: multiple query (separated by comma)

--- src/libs/algorithms/calculator.ts
+++ src/libs/algorithms/calculator.ts
@@ -2,8 +2,8 @@ const calculator = (str: string): string => {
   str = str.split(" ").join("");
   const equation = str.match(/([\d+\-*/()^]+)/);
   try {
-    const res = eval(equation ? equation[0].split("^").join("**") : "");
-    return res.toString();
+    const res = eval(equation![0].split("^").join("**"));
+    return `${equation![0].split("").join(" ")} = ${res}`;
   } catch (e) {
     return "Sintaks persamaan tidak sesuai!";
   }
